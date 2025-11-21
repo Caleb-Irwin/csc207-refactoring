ce6225d913f6524cb2a76e645e4b0f7523844ecd
Caleb Irwin <me@calebirwin.ca>
2025-11-21 17:02:58 -0500

    Task 1.2 GitLog + Add Docstring for Exception

--- src/main/java/theater/UnknownPlayTypeException.java
+++ src/main/java/theater/UnknownPlayTypeException.java
@@ -1,5 +1,8 @@
 package theater;
 
+/**
+ * This class is for a custom exception for unknown for play type handling.
+ */
 public class UnknownPlayTypeException extends RuntimeException {
     public UnknownPlayTypeException(String message) {
         super(message);
